f335cce0c5cf3a95733f802130f585402367e939
swimmingcircle119 <lampmaa22@gmail.com>
2024-03-27 16:22:51 -0700

    add live code editor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import BytePairingAlgorithm from "./bytePairing";
+import { PythonProvider } from "react-py";
+import Codeblock from "./codeBlock";
 
 function App() {
   return (
     <div className="App bg-gray-100 min-h-screen flex flex-col items-center justify-center">
       <BytePairingAlgorithm></BytePairingAlgorithm>
+      <PythonProvider>
+        <Codeblock />
+      </PythonProvider>
     </div>
   );
 }
