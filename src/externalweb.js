bf94100f50fd98e8f974e6ebea833df40f50d557
swimmingcircle119 <lampmaa22@gmail.com>
2024-04-12 16:17:35 -0700

    add external website

---
+++ src/externalweb.js
@@ -0,0 +1,12 @@
+export default function ExternalWebsite() {
+  return (
+    <iframe
+      src="https://tiktok-coloring.vercel.app/"
+      title="External Website Content"
+      width="100%"
+      height="900"
+      style={{ border: 0, overflow: "hidden" }}
+      allowFullScreen
+    ></iframe>
+  );
+}
